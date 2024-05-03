3018cec1fce26ca80a3f679d28fe5f78d9a83af5
StoyanMihaylov99 <stoqn.business@gmail.com>
2024-05-03 22:14:23 +0300

    Uploading files

---
+++ src/test/java/org/example/ichodinkov/IchoDinkovApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ichodinkov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IchoDinkovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
