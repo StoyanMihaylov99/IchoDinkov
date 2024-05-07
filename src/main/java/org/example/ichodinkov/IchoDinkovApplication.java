9d648023aa27d11c265ff3dcdf4aa639b378eabe
StoyanMihaylov99 <stoqn.business@gmail.com>
2024-05-07 21:21:26 +0300

    Adding files

---
+++ src/main/java/org/example/ichodinkov/IchoDinkovApplication.java
@@ -0,0 +1,12 @@
+package org.example.ichodinkov;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchoDinkovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchoDinkovApplication.class, args);
+	}
+
+}
